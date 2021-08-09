018e977344d2e795812485fcd7961e2e57be66c6
Jan Biedermann <jan@kursator.de>
2021-08-09 22:22:11 +0200

    use new mounting

--- ruby/test_app_preact/app/components/test_app_app.rb
+++ ruby/test_app_preact/app/components/test_app_app.rb
@@ -2,10 +2,10 @@ class TestAppApp < LucidApp::Base
   render do
     Router(hook: Preact.location_hook(props.location)) do
       Switch do
-        Route(path: '/', component: component_fun('HelloComponent'))
-        Route(path: '/ssr', component: component_fun('HelloComponent'))
-        Route(path: '/welcome', component: component_fun('WelcomeComponent'))
-        Route(component: component_fun('Page404Component'))
+        Route(path: '/', component: HelloComponent.JS[:preact_component])
+        Route(path: '/ssr', component: HelloComponent.JS[:preact_component])
+        Route(path: '/welcome', component: WelcomeComponent.JS[:preact_component])
+        Route(component: Page404Component.JS[:preact_component])
       end
     end
   end
