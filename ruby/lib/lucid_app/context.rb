a084669240782d123f0eccf4a9abd0f96ee9049c
Jan Biedermann <jan@kursator.de>
2021-08-09 22:07:21 +0200

    nano_css for context

--- ruby/lib/lucid_app/context.rb
+++ ruby/lib/lucid_app/context.rb
@@ -1,7 +1,22 @@
 module LucidApp
   module Context
     def self.create_application_context
-      Preact.create_context('LucidApplicationContext', Isomorfeus.store)
+      nano_css = `null`
+      %x{
+        let og = Opal.global;
+        nano_css = og.NanoCSS.create();
+        og.NanoCSSAddOns.rule(nano_css);
+        og.NanoCSSAddOns.sheet(nano_css);
+        og.NanoCSSAddOns.nesting(nano_css);
+        og.NanoCSSAddOns.hydrate(nano_css);
+        og.NanoCSSAddOns.unitless(nano_css);
+        og.NanoCSSAddOns.global(nano_css);
+        og.NanoCSSAddOns.keyframes(nano_css);
+        og.NanoCSSAddOns.fade_in(nano_css);
+        og.NanoCSSAddOns.fade_out(nano_css);
+        og.NanoCSSInstance = nano_css;
+      }
+      Preact.create_context('LucidApplicationContext', { iso_store: Isomorfeus.store, nano_css: nano_css })
     end
   end
 end
