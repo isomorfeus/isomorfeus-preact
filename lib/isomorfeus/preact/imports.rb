1c0b78602e177dc60d58017ab80d18d6e24a9434
Jan Biedermann <jan@kursator.de>
2021-09-03 22:40:20 +0200

    update imports

--- lib/isomorfeus/preact/imports.rb
+++ lib/isomorfeus/preact/imports.rb
@@ -13,12 +13,16 @@ module Isomorfeus
         Isomorfeus.add_common_js_import("nano-css/addon/animate/fadeIn", 'NanoCSSAddons', 'addon', 'fadeIn')
         Isomorfeus.add_common_js_import("nano-css/addon/animate/fadeOut", 'NanoCSSAddons', 'addon', 'fadeOut')
 
-        Isomorfeus.add_ssr_js_import('preact-render-to-string', 'Preact', ['renderToString'])
+        Isomorfeus.add_ssr_js_import('preact-render-to-string', 'Preact', ['render'], nil, 'renderToString')
         Isomorfeus.add_ssr_js_import('wouter-preact/static-location', 'staticLocationHook')
 
         Isomorfeus.add_web_js_import('wouter-preact/use-location', 'locationHook')
         if Isomorfeus.development?
           Isomorfeus.add_web_js_import('preact-deep-force-update', 'deepForceUpdate')
+        end
+
+        if Dir.exist?(Isomorfeus.app_root)
+          Isomorfeus.add_common_ruby_import('isomorfeus_loader') if File.exist?(File.join(Isomorfeus.app_root, 'isomorfeus_loader.rb'))
         end
       end
     end
