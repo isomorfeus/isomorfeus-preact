e34f483bca3aebd5effe65fd15620ccbadabbd38
Jan Biedermann <jan@kursator.de>
2022-01-27 20:13:54 +0100

    remve obsolete comment

--- lib/isomorfeus_preact/lucid_component/initializer.rb
+++ lib/isomorfeus_preact/lucid_component/initializer.rb
@@ -4,8 +4,6 @@ module LucidComponent
       @native = native_component
       @app_store = LucidComponent::AppStoreProxy.new(self)
       @class_store = LucidComponent::ClassStoreProxy.new(self.class.to_s, self, @native)
-      # @iso_store = Isomorfeus::IsomorphicStoreProxy.new(self)
-      # @local_store = Isomorfeus::LocalStoreProxy.new(self)
       @store = LucidComponent::InstanceStoreProxy.new(self)
       @props = `Opal.Preact.Props.$new(#@native)`
       @state = `Opal.Preact.State.$new(#@native)`
