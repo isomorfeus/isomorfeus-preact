742f83a12899d02843e37a7e2fdd3918c64d3ad7
Jan Biedermann <jan@kursator.de>
2022-02-03 17:25:22 +0100

    adapt for better styles hmr

--- lib/isomorfeus_preact/lucid_app/api.rb
+++ lib/isomorfeus_preact/lucid_app/api.rb
@@ -5,15 +5,33 @@ module LucidApp
         def theme(theme_hash = nil, &block)
           theme_hash = block.call if block_given?
           if theme_hash
+            component_name = self.to_s
             %x{
-              let css;
-              if (typeof theme_hash.$to_n === 'function') { css = theme_hash.$to_n(); }
-              else { css = theme_hash; }
-              let nano_styles = Opal.global.NanoCSSInstance.sheet(css, "LucidAppTheme");
-              base.css_theme = #{::LucidComponent::StylesWrapper.new(`nano_styles`)};
+              let rule_name = component_name.replace(/:/g, '_');
+              let ogni = Opal.global.NanoCSSInstance;
+              if (base.css_theme && #{Isomorfeus.production?}) { return base.css_theme; }
+              else if(#{Isomorfeus.development?}) {
+                if (#{on_browser?}) {
+                  ogni.delete_from_sheet(rule_name);
+                  ogni.delete_from_rule_blocks(rule_name);
+                  ogni.hydrate_force_put = true;
+                }
+              }
+              if (typeof theme_hash.$is_wrapped_style !== 'undefined') {
+                base.css_theme = theme_hash;
+              } else {
+                let css;
+                if (typeof theme_hash.$to_n === 'function') { css = theme_hash.$to_n(); }
+                else { css = theme_hash; }
+                let nano_styles = ogni.sheet(css, rule_name);
+                base.css_theme = #{::LucidComponent::StylesWrapper.new(`nano_styles`)};
+              }
             }
           end
-          `base.css_theme`
+          %x{
+            if (!base.css_theme) { return nil; }
+            return base.css_theme;
+          }
         end
         alias_method :theme=, :theme
       end
